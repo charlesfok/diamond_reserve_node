fafa9d9db91a65a14c4ded112bc0479f17f98d38
charles <charlesfok888@gmail.com>
2017-12-06 15:46:39 +0800

    Update fancy_color

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ app.get('/', function(req, res) {
   res.send('Hello World!')
 })
 
+// Basic 404 handler
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+
+// Basic error handler
+app.use((err, req, res, next) => {
+
+  console.error(err);
+  err.response = {
+    message: err.message,
+    internalCode: err.code
+  };
+  res.status(500).send(err.response || 'Something broke!');
+});
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
